24fb5e3d44d9fd85a8e69cbddb33d021bab9f361
beytullahgurpinar <beytullahgurpinar@teknasyon.com>
2020-08-20 10:22:17 +0300

    Profile response add new parameters.

--- examples/subscriberProfile.php
+++ examples/subscriberProfile.php
@@ -7,14 +7,14 @@ use Zotlo\Connect\Entity\Credentials;
 use Zotlo\Connect\Entity\Request;
 
 $credentials = new Credentials();
-$credentials->setAccessKey("0ba673ed73fc17c9f739a2ba4daaf6afec95816b94f7d9fd4e")->setAccessSecurity("b1c8202410a10479134710dd8c06f8a248e68723ea898688817ef86dd1d9d85f5db5b0e68c3dc6c4")->setApplicationId('2');
+$credentials->setAccessKey("1")->setAccessSecurity("1")->setApplicationId('2');
 
 $subscriber = new \Zotlo\Connect\Entity\Subscriber();
-$subscriber->setSubscriberId('905389837455');
+$subscriber->setSubscriberId('testtest');
 
 $request = new Request();
 $request->setPlatform('web');
-$request->setEndpoint('https://api-zotlo.mobylonia.com/');
+$request->setEndpoint('http://api.zotlo.localhost/');
 $request->setLanguage('en');
 
 
@@ -24,7 +24,7 @@ $client->subscription()->setRequest($request);
 
 try {
     $response = $client->subscription()->profile();
-    print_r($response->getProfile());
+    print_r($response);
 
 } catch (\Zotlo\Connect\Exception\PaymentException $exception) {
     echo $exception->getErrorCode() . PHP_EOL;
